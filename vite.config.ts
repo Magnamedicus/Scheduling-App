a485c3c873ad4e0e261ba026146be43f664d0928
Peter Hornbach <BSNPete@gmail.com>
2025-09-10 17:24:51 -0400

    added netlify dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: "/",
   build: {
     outDir: 'dist',
   },
-  
+
 });

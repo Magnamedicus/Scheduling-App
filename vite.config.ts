da247d5e869a390a3c36bb873d6876ddda8b9bf4
Peter Hornbach <BSNPete@gmail.com>
2025-09-12 17:23:55 -0400

    modified files for railway deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/",
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: '0.0.0.0'
+  },
   build: {
     outDir: 'dist',
-  },
-
-});
+    emptyOutDir: true
+  }
+})
